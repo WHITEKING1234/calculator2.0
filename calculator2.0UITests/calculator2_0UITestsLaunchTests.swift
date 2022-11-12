61a4d9ae151346d59fa084702aba1c4a18aaa721
bombag456 <bombag456@gmail.com>
2022-11-13 02:28:59 +0600

    Initial Commit

---
+++ calculator2.0UITests/calculator2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  calculator2_0UITestsLaunchTests.swift
+//  calculator2.0UITests
+//
+//  Created by Mac on 13/11/22.
+//
+
+import XCTest
+
+final class calculator2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
